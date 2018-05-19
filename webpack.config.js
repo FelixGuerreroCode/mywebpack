77dcb1f28649e4f64e9d9264613d16774db81afc
FelixG <felix.guerrero.clan@gmail.com>
2018-05-19 09:07:27 -0700

    Fixed styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
@@ -17,14 +18,15 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
-				use: [{
-					loader: 'style-loader'
-				}, {
-					loader: 'css-loader'
-				}, {
-					loader: 'sass-loader'
-				}]
+				use: ExtractTextPlugin.extract(
+					{
+						fallback: 'style-loader',
+						use: ['css-loader','sass-loader' ]
+					})
 			}
 		]
-	}
+	},
+	plugins: [ 
+	    new ExtractTextPlugin( { filename: 'style.css' } )
+	  ]
 };
