d3342f9475f37b2db908c271625e254a4b29ed79
FelixG <felix.guerrero.clan@gmail.com>
2018-05-19 00:16:41 -0700

    css parser

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const root = document.createElement("div")
-root.innerHTML = `<p>Hello Webpack.</p>`
-document.body.appendChild(root)
+import R, {forEach,map,add} from 'ramda';
+import testingArray from "./people";
+import "./style.scss";
+
+var finalValue = map( add( 1 ), testingArray );
+console.log( finalValue );
+
+const root = document.createElement("div");
+root.innerHTML = "<p>Hello Webpack whatever for now.</p>";
+document.body.appendChild(root);
---
+++ src/people.js
@@ -0,0 +1,5 @@
+const testingArray = [
+	1, 2, 3
+];
+
+export default testingArray;
