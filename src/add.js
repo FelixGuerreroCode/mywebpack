a0161edcd334c81a89859182ba8ac24eb52b5b25
FelixG <felix.guerrero.clan@gmail.com>
2018-06-17 07:04:12 -0700

    prior to remote repo setup

--- src/add.js
+++ src/add.js
@@ -1,8 +1,52 @@
 const R = require( "ramda" );
+const fs = require( "fs" );
+
+const readFile = path => fs.readFileSync( path, { encoding : "utf-8" } );
+
+function isTrue( x ){
+	return x;
+}
+
+function factorial( num ){
+	var nextNumber = num - 1;
+	if( nextNumber === 1 ){
+		console.log( "calculation :>>>>>>> " + num );
+		return num;
+	}
+	var mult =  num * factorial( nextNumber );
+	console.log( "calculation : " + num  + "next " + nextNumber );
+	return mult;
+}
 
 const add = ( a, b ) => {
-	var isString = R.is( String );
-	console.log( "testing: " + isString( "testing" ) );
+	var maxNumber = 5;
+	var theFactorial = factorial( maxNumber );
+	console.log( ">>>>>>>>>>>> " + theFactorial );
+
+
+	var names = [ " ONe", " TWO ", " three  " ];
+	var statusArray = [ true, false, true ];
+
+	R.pipe( 
+		R.map( R.complement( isTrue ) ),
+		console.log )( statusArray );
+
+	R.pipe( 
+		R.map( R.trim ), 
+		R.map( R.toLower ), 
+		console.log )( names );
+
+	var files =  [ './src/files/bitcoin.csv', './src/files/tether.csv'];
+	var triggerCommandsOn = R.pipe( 
+		R.map( readFile ),
+		R.map( R.dropLast( 1 ) ),
+		R.map( R.split( "\n" ) ),
+		R.map( R.map( R.split( "\t" ) ) ),
+		R.map( R.map( R.drop( 1 ) ) ),
+		console.log 
+	);
+
+	//triggerCommandsOn( files );
 	return a + b;	
 };
 
